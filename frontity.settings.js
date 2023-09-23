2a9be331863931ce5aad5af72171456fd646d603
Ankur <abishek.singh.chauhan@gmail.com>
2023-09-23 21:45:17 +0545

    Initializing new repo for sawtee website project

---
+++ frontity.settings.js
@@ -0,0 +1,79 @@
+const settings = {
+  name: "sawtee-frontity",
+  state: {
+    frontity: {
+      url: "https://sawtee.org/backend",
+      title: "SAWTEE",
+      description: "South Asia Watch on Trade, Economics and Environment",
+      year: "1994",
+      logo: "/assets/logo-sawtee.webp",
+    },
+  },
+  packages: [
+    {
+      name: "frontity-chakra-theme",
+      state: {
+        theme: {
+          menu: [
+            ["Home", "/"],
+            ["Nature", "/category/nature/"],
+            ["Travel", "/category/travel/"],
+            ["Japan", "/tag/japan/"],
+            ["About Us", "/about-us/"],
+          ],
+          socialLinks: [
+            ["facebook", "https://www.facebook.com/sawteenp/"],
+            ["twitter", "https://www.twitter.com/SAWTEENP/"],
+            ["linkedin", "https://www.linkedin.com/sawteenp/"],
+            ["youtube", "https://www.youtube.com/@sawteenp/"],
+          ],
+          featured: {
+            showOnList: false,
+            showOnPost: false,
+          },
+        },
+      },
+    },
+    {
+      name: "@frontity/wp-source",
+      state: {
+        source: {
+          url: "https://sawtee.org/backend",
+          homepage: "home",
+          postsPage: "blog",
+          params: {
+            orderBy: "id",
+            order: "desc",
+            per_page: 10,
+          },
+          postTypes: [
+            {
+              type: "featured-events",
+              endpoint: "featured-events",
+              archive: "/featured-events",
+            },
+            {
+              type: "publications",
+              endpoint: "publications",
+              archive: "/publications",
+            },
+            {
+              type: "newsletters",
+              endpoint: "newsletters",
+              archive: "/newsletters",
+            },
+            {
+              type: "research",
+              endpoint: "research",
+              archive: "/research",
+            },
+          ],
+        },
+      },
+    },
+    "@frontity/tiny-router",
+    "@frontity/html2react",
+  ],
+};
+
+export default settings;
